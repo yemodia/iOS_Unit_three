ca69f4209198bceb8202b9c800eb6766e09e7b8b
Yerim <yerim@iMac6.local>
2019-05-03 11:03:46 -0400

    About Me Lab. In this lab i used and made the difference between view controllers and tab bar controllers.

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Yerim on 5/2/19.
+//  Copyright © 2019 Yerim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
